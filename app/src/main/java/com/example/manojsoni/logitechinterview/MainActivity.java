9740a6f75db5ad892d8241257bf587b68654eea9
itsmanojsoni <itsmanojsoni@gmail.com>
2019-01-05 11:51:39 -0800

    First Commit

---
+++ app/src/main/java/com/example/manojsoni/logitechinterview/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.manojsoni.logitechinterview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import io.reactivex.Observable;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        callThisMethod();
+    }
+
+    private ArrayList<Long>  callThisMethod() {
+
+
+        long[] output = getFibNum(100);
+
+        for (int i = 0; i < output.length; i++) {
+
+            Log.d(TAG, "output = "+output[i]);
+        }
+
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+    public native long[] getFibNum (int num);
+}
